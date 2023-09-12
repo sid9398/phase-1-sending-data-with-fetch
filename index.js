a620f3b28a120a59cf1cc7ebe60918a7f40c6eb9
SidneyKing <sid8121@gmail.com>
2023-09-11 20:54:11 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email){
+    const formData = {
+        name, 
+        email
+      }
+      
+      const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+      }
+      return fetch("http://localhost:3000/users", configurationObject)
+        .then(function (response) {
+          return response.json()
+        })
+        .then(function (object) {
+          document.body.innerHTML = object.id
+        })
+        .catch(function (error) {
+            document.body.innerHTML = error.message
+          })
+}
